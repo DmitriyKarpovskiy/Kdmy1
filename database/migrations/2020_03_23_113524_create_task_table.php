bdbeaed0825b203a6d9e81d58987f508b2cdab1e
Dmitriy Karpovskiy <dmkarpovskiy@gmail.com>
2020-03-23 15:49:54 +0200

    initial commit

---
+++ database/migrations/2020_03_23_113524_create_task_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTaskTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('task', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->integer('order');
+            $table->boolean('status')->default(false);
+            $table->unsignedBigInteger('project_id');
+            $table->foreign('project_id')->references('id')->on('project')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('task');
+    }
+}
