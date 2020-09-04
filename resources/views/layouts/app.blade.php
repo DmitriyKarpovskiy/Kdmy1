18f36103114e9c542db3661ba24e77720af10865
Dmitriy Karpovskiy <dmkarpovskiy@gmail.com>
2020-09-04 17:03:54 +0300

    Menu items style is changed

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -39,8 +39,8 @@
                     <ul class="nav navbar-nav navbar-right">
                         <!-- Authentication Links -->
                         @guest
-                            <li><a href="{{ route('login') }}">Login</a></li>
-                            <li><a href="{{ route('register') }}">Register</a></li>
+                            <li><a class="menu_item" href="{{ route('login') }}">Login</a></li>
+                            <li><a class="menu_item" href="{{ route('register') }}">Register</a></li>
                         @else
                                 <a href="#" id="dropdown" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false" aria-haspopup="true" v-pre>
                                     {{ Auth::user()->name }} <span class="caret"></span>
